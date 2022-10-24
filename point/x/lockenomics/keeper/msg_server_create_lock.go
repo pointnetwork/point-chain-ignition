af844cc609d1972f6bcd4a99c49b380e355c5a4c
Iury V <discours@list.ru>
2022-10-24 16:41:58 +0300

    delegationLock logic in progress

--- point/x/lockenomics/keeper/msg_server_create_lock.go
+++ point/x/lockenomics/keeper/msg_server_create_lock.go
@@ -11,6 +11,28 @@ func (k msgServer) CreateLock(goCtx context.Context, msg *types.MsgCreateLock) (
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	// TODO: Handling the message
+	/*valAddr, valErr := sdk.ValAddressFromBech32(msg.Validator)
+	if valErr != nil {
+		return nil, valErr
+	}
+
+	validator, found := k.stakingKeeper.GetValidator(ctx, valAddr)
+
+	if !found {
+		return nil, stakingTypes.ErrNoValidatorFound
+	}
+
+	delegatorAddress, err := sdk.AccAddressFromBech32(msg.Creator)
+	if err != nil {
+		return nil, err
+	}
+
+	err := k.Keeper.SetDelegationLock(ctx, )
+	if err != nil {
+		return nil, err
+	}*/
+
+	// TODO: Handling the message end
 	_ = ctx
 
 	return &types.MsgCreateLockResponse{}, nil
